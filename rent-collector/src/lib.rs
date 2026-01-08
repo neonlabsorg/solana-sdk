a4cc056b2bb4854ac3e57b261698f54c3b9e5b9b
Vladimir Bugaev <boogvar@gmail.com>
2026-01-08 15:31:28 +0700

    set RENT_EXEMPT_RENT_EPOCH

--- rent-collector/src/lib.rs
+++ rent-collector/src/lib.rs
@@ -45,7 +45,7 @@ impl Default for RentCollector {
 /// When rent is collected from an exempt account, rent_epoch is set to this
 /// value. The idea is to have a fixed, consistent value for rent_epoch for all accounts that do not collect rent.
 /// This enables us to get rid of the field completely.
-pub const RENT_EXEMPT_RENT_EPOCH: Epoch = 0;
+pub const RENT_EXEMPT_RENT_EPOCH: Epoch = Epoch::MAX;
 
 /// when rent is collected for this account, this is the action to apply to the account
 #[allow(dead_code)]
