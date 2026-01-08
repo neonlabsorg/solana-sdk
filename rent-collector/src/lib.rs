b146bf200621694c0df151fb11db0d960c4c7a72
Vladimir Bugaev <boogvar@gmail.com>
2026-01-08 15:13:11 +0700

    rent-collector

--- rent-collector/src/lib.rs
+++ rent-collector/src/lib.rs
@@ -6,6 +6,7 @@
     note = "Use Rent directly, or solana_runtime::rent_collector"
 )]
 
+#[allow(unused_imports)]
 use {
     solana_account::{AccountSharedData, ReadableAccount, WritableAccount},
     solana_clock::Epoch,
@@ -44,9 +45,10 @@ impl Default for RentCollector {
 /// When rent is collected from an exempt account, rent_epoch is set to this
 /// value. The idea is to have a fixed, consistent value for rent_epoch for all accounts that do not collect rent.
 /// This enables us to get rid of the field completely.
-pub const RENT_EXEMPT_RENT_EPOCH: Epoch = Epoch::MAX;
+pub const RENT_EXEMPT_RENT_EPOCH: Epoch = 0;
 
 /// when rent is collected for this account, this is the action to apply to the account
+#[allow(dead_code)]
 #[derive(Debug)]
 enum RentResult {
     /// this account will never have rent collected from it
@@ -65,13 +67,13 @@ impl RentCollector {
         epoch: Epoch,
         epoch_schedule: EpochSchedule,
         slots_per_year: f64,
-        rent: Rent,
+        _rent: Rent,
     ) -> Self {
         Self {
             epoch,
             epoch_schedule,
             slots_per_year,
-            rent,
+            rent: Rent::default(),
         }
     }
 
@@ -83,40 +85,42 @@ impl RentCollector {
     }
 
     /// true if it is easy to determine this account should consider having rent collected from it
-    pub fn should_collect_rent(&self, address: &Pubkey, executable: bool) -> bool {
-        !(executable // executable accounts must be rent-exempt balance
-            || *address == incinerator::id())
+    pub fn should_collect_rent(&self, _address: &Pubkey, _executable: bool) -> bool {
+        false
+        // !(executable // executable accounts must be rent-exempt balance
+        //     || *address == incinerator::id())
     }
 
     /// given an account that 'should_collect_rent'
     /// returns (amount rent due, is_exempt_from_rent)
     pub fn get_rent_due(
         &self,
-        lamports: u64,
-        data_len: usize,
-        account_rent_epoch: Epoch,
+        _lamports: u64,
+        _data_len: usize,
+        _account_rent_epoch: Epoch,
     ) -> RentDue {
-        if self.rent.is_exempt(lamports, data_len) {
-            RentDue::Exempt
-        } else {
-            let slots_elapsed: u64 = (account_rent_epoch..=self.epoch)
-                .map(|epoch| {
-                    self.epoch_schedule
-                        .get_slots_in_epoch(epoch.saturating_add(1))
-                })
-                .sum();
+        RentDue::Exempt
+        // if self.rent.is_exempt(lamports, data_len) {
+        //     RentDue::Exempt
+        // } else {
+        //     let slots_elapsed: u64 = (account_rent_epoch..=self.epoch)
+        //         .map(|epoch| {
+        //             self.epoch_schedule
+        //                 .get_slots_in_epoch(epoch.saturating_add(1))
+        //         })
+        //         .sum();
 
-            // avoid infinite rent in rust 1.45
-            let years_elapsed = if self.slots_per_year != 0.0 {
-                slots_elapsed as f64 / self.slots_per_year
-            } else {
-                0.0
-            };
+        //     // avoid infinite rent in rust 1.45
+        //     let years_elapsed = if self.slots_per_year != 0.0 {
+        //         slots_elapsed as f64 / self.slots_per_year
+        //     } else {
+        //         0.0
+        //     };
 
-            // we know this account is not exempt
-            let due = self.rent.due_amount(data_len, years_elapsed);
-            RentDue::Paying(due)
-        }
+        //     // we know this account is not exempt
+        //     let due = self.rent.due_amount(data_len, years_elapsed);
+        //     RentDue::Paying(due)
+        // }
     }
 
     // Updates the account's lamports and status, and returns the amount of rent collected, if any.
@@ -161,34 +165,35 @@ impl RentCollector {
     #[must_use]
     fn calculate_rent_result(
         &self,
-        address: &Pubkey,
-        account: &impl ReadableAccount,
+        _address: &Pubkey,
+        _account: &impl ReadableAccount,
     ) -> RentResult {
-        if account.rent_epoch() == RENT_EXEMPT_RENT_EPOCH || account.rent_epoch() > self.epoch {
-            // potentially rent paying account (or known and already marked exempt)
-            // Maybe collect rent later, leave account alone for now.
-            return RentResult::NoRentCollectionNow;
-        }
-        if !self.should_collect_rent(address, account.executable()) {
-            // easy to determine this account should not consider having rent collected from it
-            return RentResult::Exempt;
-        }
-        match self.get_rent_due(
-            account.lamports(),
-            account.data().len(),
-            account.rent_epoch(),
-        ) {
-            // account will not have rent collected ever
-            RentDue::Exempt => RentResult::Exempt,
-            // potentially rent paying account
-            // Maybe collect rent later, leave account alone for now.
-            RentDue::Paying(0) => RentResult::NoRentCollectionNow,
-            // Rent is collected for next epoch.
-            RentDue::Paying(rent_due) => RentResult::CollectRent {
-                new_rent_epoch: self.epoch.saturating_add(1),
-                rent_due,
-            },
-        }
+        return RentResult::NoRentCollectionNow;
+        // if account.rent_epoch() == RENT_EXEMPT_RENT_EPOCH || account.rent_epoch() > self.epoch {
+        //     // potentially rent paying account (or known and already marked exempt)
+        //     // Maybe collect rent later, leave account alone for now.
+        //     return RentResult::NoRentCollectionNow;
+        // }
+        // if !self.should_collect_rent(address, account.executable()) {
+        //     // easy to determine this account should not consider having rent collected from it
+        //     return RentResult::Exempt;
+        // }
+        // match self.get_rent_due(
+        //     account.lamports(),
+        //     account.data().len(),
+        //     account.rent_epoch(),
+        // ) {
+        //     // account will not have rent collected ever
+        //     RentDue::Exempt => RentResult::Exempt,
+        //     // potentially rent paying account
+        //     // Maybe collect rent later, leave account alone for now.
+        //     RentDue::Paying(0) => RentResult::NoRentCollectionNow,
+        //     // Rent is collected for next epoch.
+        //     RentDue::Paying(rent_due) => RentResult::CollectRent {
+        //         new_rent_epoch: self.epoch.saturating_add(1),
+        //         rent_due,
+        //     },
+        // }
     }
 }
 
